1167bb212176d2e5f0dbd7da6a39442c60f8c4f6
Navneet Dhody <navneet.dhody@gmail.com>
2022-05-26 07:47:54 +0800

    feat(bash): fzf to use fd

--- bash/.bashrc.d/97-fzf.sh
+++ bash/.bashrc.d/97-fzf.sh
@@ -1,1 +1,5 @@
 source /usr/share/fzf/shell/key-bindings.bash
+
+export FZF_DEFAULT_COMMAND="fd --follow --exclude .git"
+export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+export FZF_ALT_C_COMMAND="fd --type d --follow --exclude .git"
