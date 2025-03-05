01fc8d963c3d7757c14579a4703c1c002cb1dbe9
Navneet Dhody <navneet.dhody@gmail.com>
2025-03-05 14:54:38 +0800

    feat(bash): update bash configuration

--- bash/.bashrc.d/67-fzf.sh
+++ bash/.bashrc.d/67-fzf.sh
@@ -1,7 +1,10 @@
 [[ ! $(command -v fzf) ]] && return
 
 eval "$(fzf --bash)"
-export FZF_DEFAULT_COMMAND="fd --follow --exclude .git"
-export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-export FZF_ALT_C_COMMAND="fd --type d --follow --exclude .git"
+
+if [[ $(command -v fd) ]]; then
+    export FZF_DEFAULT_COMMAND="fd --follow --exclude .git"
+    export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+    export FZF_ALT_C_COMMAND="fd --type d --follow --exclude .git"
+fi
 
