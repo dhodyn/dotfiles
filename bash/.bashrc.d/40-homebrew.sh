d0b2cbf59ff0ad01d27ed5d26fb7e72560b25267
Navneet Dhody <navneet.dhody@gmail.com>
2025-03-11 17:25:37 +0800

    update

--- bash/.bashrc.d/40-homebrew.sh
+++ bash/.bashrc.d/40-homebrew.sh
@@ -1,5 +1,5 @@
-if [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    export HOMEBREW_BUNDLE_FILE="${XDG_CONFIG_HOME-$HOME/.config}/homebrew-bundle/Brewfile"
-fi
+[[ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]] && return
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+export HOMEBREW_BUNDLE_FILE="${XDG_CONFIG_HOME-$HOME/.config}/homebrew-bundle/Brewfile"
 
