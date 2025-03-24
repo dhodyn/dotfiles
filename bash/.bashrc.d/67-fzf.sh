ce0dd8a431442c4de867415496215bf9defaaa69
Navneet Dhody <navneet.dhody@gmail.com>
2025-03-24 21:39:43 +0800

    update fzf

--- bash/.bashrc.d/67-fzf.sh
+++ bash/.bashrc.d/67-fzf.sh
@@ -2,9 +2,3 @@
 
 eval "$(fzf --bash)"
 
-if [[ $(command -v fd) ]]; then
-    export FZF_DEFAULT_COMMAND="fd --follow --exclude .git"
-    export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-    export FZF_ALT_C_COMMAND="fd --type d --follow --exclude .git"
-fi
-
