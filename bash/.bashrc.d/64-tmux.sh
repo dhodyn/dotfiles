477a5b0a60fab4f0709cbc9699ce083b136dc2e4
Navneet Dhody <navneet.dhody@gmail.com>
2026-07-17 14:55:21 +0800

    feat(bash): update bash configuration

--- bash/.bashrc.d/64-tmux.sh
+++ bash/.bashrc.d/64-tmux.sh
@@ -1,8 +1,8 @@
 [[ ! $(command -v tmux) ]] && return
 
-# if [[ -z ${TMUX} ]]; then
-#     exec tmux attach-session
-# fi
+if [[ -z ${TMUX} ]]; then
+    exec tmux attach-session
+fi
 
 export TMUX_SCREEN_TERM=0
 export TMUX_NO_TRUECOLOR=0
